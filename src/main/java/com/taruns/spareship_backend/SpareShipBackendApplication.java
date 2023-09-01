d3ebf2e3c33c0b7c61651dbbb5d16cbcaa85c454
Tarun-108 <tarunstu125@gmail.com>
2023-09-01 18:19:13 +0530

    Project Initialized

---
+++ src/main/java/com/taruns/spareship_backend/SpareShipBackendApplication.java
@@ -0,0 +1,14 @@
+package com.taruns.spareship_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpareShipBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpareShipBackendApplication.class, args);
+        System.out.println("The Start");
+    }
+
+}
---
+++ src/test/java/com/taruns/spareship_backend/SpareShipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taruns.spareship_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpareShipBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
